05040ca73e233e8376288348bf5dd4ba7525d0d2
Spenc-Boo-e <62921993+Spenc-Boo-e@users.noreply.github.com>
2020-06-12 23:17:15 +1000

    Add files via upload

---
+++ HTML/JAVA/mobilescript.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './Design.css';
+
+window.setMobileTable = function(selector) {
+  // if (window.innerWidth > 600) return false;
+  const tableEl = document.querySelector(selector);
+  const thEls = tableEl.querySelectorAll('thead th');
+  const tdLabels = Array.from(thEls).map(el => el.innerText);
+  tableEl.querySelectorAll('tbody tr').forEach( tr => {
+    Array.from(tr.children).forEach( 
+      (td, ndx) =>  td.setAttribute('label', tdLabels[ndx])
+    );
+  });
+}
